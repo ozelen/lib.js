d28a1f8346819cdf4182b7a9e113b631e9e7f81d
Oleksiy Zelenyuk <oleksiy.zelenyuk@symphony-solutions.eu>
2015-09-20 09:36:12 +0300

    nitpick fix

--- lib.js
+++ lib.js
@@ -242,9 +242,7 @@ function checker() {
 
     return function(obj) {
         return validators.reduce(function(errs, check) {
-            if (check(obj)) return errs;
-            else
-                return (errs.push(check.message), errs);
+            return check(obj) ? errs : errs.concat([check.message]);
         }, []);
     };
 }
